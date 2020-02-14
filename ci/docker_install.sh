e2cc91f7f845b6a8b445dd3a96c872f0e16d6506
Ricardo Cruz <piradoiv@gmail.com>
2020-02-14 19:19:24 +0000

    Initial commit

---
+++ ci/docker_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+apt-get update -yqq
+apt-get install -yqq libcurl4-openssl-dev pkg-config libssl-dev
+
+curl --location --output /usr/local/bin/composer https://getcomposer.org/composer-stable.phar
+
